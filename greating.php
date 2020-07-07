727cb6b8470ba35264899583b96aa83c97b7b01b
Mohammed Usman E Gani <ka.usmanegani@gmail.com>
2020-07-07 22:39:35 +0530

    Initial Commit

---
+++ greating.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ramzan | 2019</title>
+    <link rel="icon" href="images/title_logo.png">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="container"> 
+        <div class="title">
+            <h1>Assalamualaikum <?php echo $_POST['name-of-user'];?></h1>
+        </div>
+        <div class="title-description">
+            <h2>May Allah grant all your wishes and accept your sacrifice. May he fulfill all your desires in life. I wish happiness for you and your family today and always.( ! AMEEN ! ) </h2>
+        </div>
+        <div class="title-description">
+            <h2>! Eid-Mubarak !</h2>
+        </div>
+        <a href="index.html">Go To Back</a>
+    </div>
+    <footer>
+        <h3>Developed By : Usman</h3>
+    </footer>
+</body>
+</html>
